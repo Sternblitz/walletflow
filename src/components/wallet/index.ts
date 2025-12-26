95e7d43109755438a619cd48e46a24f56f0a3320
Sternblitz <info@sternblitz.de>
2025-12-26 22:49:28 +0300

    Revert "chore: Remove preview/wallet and debug endpoints"
    This reverts commit c88b2aa5d8a6355c447fe9e28816cd4bdf63a2d2.

---
+++ src/components/wallet/index.ts
@@ -0,0 +1,13 @@
+// Wallet Editor Components - Export Index
+
+export { TemplatePicker } from './template-picker'
+export { PassPreview } from './pass-preview'
+export { PassEditor } from './pass-editor'
+export { FieldsEditor } from './fields-editor'
+export { ColorsEditor } from './colors-editor'
+export { ImagesEditor } from './images-editor'
+export { SettingsEditor } from './settings-editor'
+
+// Legacy components (for backwards compatibility)
+export { ApplePassPreview } from './apple-pass-preview'
+export { WalletSimulator } from './wallet-simulator'
